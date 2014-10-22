47abb5cfdc5dad39861fc3afbb1976fe4643393a
Garrett Smith <garrettwhsmith@gmail.com>
2014-10-21 20:33:47 -0500

    Moved over to angular-seed, Ionic isn't a good fit because this isn't a web app

--- www/js/controllers.js
+++
@@ -1,8 +0,0 @@
-angular.module('portfolio.controllers', [])
-
-.controller('AppCtrl', function($scope) {
-})
-
-.controller('ProjectsCtrl', function($scope, Projects) {
-  $scope.projects = Projects.all();
-});
